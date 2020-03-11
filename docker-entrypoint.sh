6871945e628f063ed322d405adc5e9b136a18b03
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-11 19:08:30 +0900

    Add debugging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,7 +12,9 @@ then
     paramArray+=("-p")
     paramArray+=($4)
 fi
+echo $CQLSH_VERSION
 paramArray+=("--cqlversion=\"$CQLSH_VERSION\"")
+echo "Running: cqlsh ${paramArray[@]}"
 echo "CREATE KEYSPACE IF NOT EXISTS $5 WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};" | cqlsh "${paramArray[@]}"
 if [[ $? -ne 0 ]]
 then
