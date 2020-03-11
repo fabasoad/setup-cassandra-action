7857c504e084d04840deffd9d295fb57aed64b4a
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-11 19:00:51 +0900

    Add exit codes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,7 +13,15 @@ then
     paramArray+=($4)
 fi
 echo "CREATE KEYSPACE IF NOT EXISTS $5 WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};" | cqlsh "${paramArray[@]}"
+if [[ $? -ne 0 ]]
+then
+  exit 1
+fi
 for file_script in $6/*.cql; do
   cqlsh -k $5 -f $file_script "${paramArray[@]}"
+  if [[ $? -ne 0 ]]
+  then
+    exit 1
+  fi
   echo "${file_script} had been run"
 done
